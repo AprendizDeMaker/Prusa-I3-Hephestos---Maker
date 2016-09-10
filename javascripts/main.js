6322eb5755180bbfd7fff6b9ebb4b7c8d251cd4c
AprendizDeMaker <netgames.andurino@gmail.com>
2016-09-10 11:56:02 +0200

    Añado JS función ponerIdioma

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
 console.log('This would be the main JS file.');
+
+var idioma = "hispano";
+function ponerIdioma(cual)	{
+	document.getElementById(idioma).style.display = "none";
+	idioma = cual;
+	document.getElementById(idioma).style.display = "block";
+}
