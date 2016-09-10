11644b35e80e92e4fe91b16e22f12120b9227cb2
AprendizDeMaker <netgames.andurino@gmail.com>
2016-09-10 11:50:49 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
 console.log('This would be the main JS file.');
+var idioma = "hispano";
+function ponerIdioma(cual)	{
+	document.getElementById(idioma).style.display = "none";
+	idioma = cual;
+	document.getElementById(idioma).style.display = "block";
+}
